f23bb16bb5cb4699c81bb4c0b96779a70a805176
Mshivangi0 <shivangim112@gmail.com>
2023-08-08 15:45:12 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+const Hero= ()=>{
+    return <main className="Hero">
+        <div className="Hero-content">
+<h1>YOUR FEET 
+DESERVE
+THE BEST</h1>
+<p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+      <div className="Hero-button">
+      <button>Shop Now</button>
+<button className="secondary-but">Category</button>
+      </div> 
+      <div className="Shopping">
+        <p>Also Available On</p>
+      </div>
+      <div className="brand-icon">
+        <img src="/images/amazon.png" alt="amazone-icon" />
+        <img src="/images/flipkart.png" alt="flipkart-icon" />
+      </div>
+       </div>
+        <div className="Hero-image">
+        <img src="/images/hero-image.png" alt="hero-icon" />
+        </div>
+    </main>;
+};
+export default Hero;
